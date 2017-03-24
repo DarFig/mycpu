447ee0fe7aa37293d2ae6ba47c0b85ec77f98863
Napsterdos <log.Dariel@gmail.com>
2017-03-24 22:53:14 +0100

    add BancoRegistros

--- c_modules/BancoRegistros.h
+++ c_modules/BancoRegistros.h
@@ -3,8 +3,17 @@
 #include <stdint.h>
 
 static const uint32_t max_reg = 32;
+static uint32_t banco [max_reg];
 
- 
+/*
+Las dreciones tienen que esta dentro del dominio de max_reg
+*/
+void w_port(uint8_t num_reg, uint32_t dato){
+  banco[num_reg] = dato;
+}
+uint32_t r_port(uint8_t num_reg){
+  return banco[num_reg];
+}
 
 
 #endif
--- c_modules/utils.h
+++ c_modules/utils.h
@@ -2,7 +2,18 @@
 #define UTILS_H
 #include <stdint.h>
 
-
+uint32_t multiplex_4(uint32_t in0, uint32_t in1, uint32_t in2, uint32_t in3, int selec){
+  if(selec == 0)
+    return in0;
+  if(selec == 1)
+    return in1;
+  if(selec == 2)
+    return in2;
+  if(selec == 3)
+    return in3;
+  if(selec > 3)
+    return 0;
+}
 
 
 #endif
